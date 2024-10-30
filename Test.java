d33a6dc1aa45be7ce05812b19c40c17f71228f05
packjack <83289186+Packjackisback@users.noreply.github.com>
2024-10-30 08:00:28 -0500

    Reorganizing
    Putting images into the assets folder

--- Test.java
+++ Test.java
@@ -9,16 +9,26 @@ import java.util.HashMap;
 import java.util.List;
 
 public class Test {
+    private static HashMap<String, String> lookup = new HashMap<>();
+    static {
+        lookup.put("w", "White");
+        lookup.put("k", "Black");
+        lookup.put("u", "Blue");
+        lookup.put("r", "Red");
+        lookup.put("g", "Green");
+    }
     public void createCards(String filename) {
         List<List<String>> records = new ArrayList<>();
         try (BufferedReader br = new BufferedReader(new FileReader("book.csv"))) {
-            String line;
+            String line = br.readLine();
+            br.readLine();
+
             while ((line = br.readLine()) != null) {
                 String[] values = line.split(",");
-                HashMap<String, Integer> cost = new HashMap<String, Integer>();
+                HashMap<Gem, Integer> cost = new HashMap<String, Integer>();
                 List<String> names = Arrays.asList(values[3].split("+"));
                 for(String name : names) {
-                    cost.put(new Gem(name.substring(1,2)), Integer.parseInt(name.substring(0,1)), )
+                    cost.put(new Gem(lookup.get(name.substring(1,2))), Integer.parseInt(name.substring(0,1)));
                 }
                 Card c = new Card(values[2], cost, values[0], new Gem(values[1]));
             }
@@ -27,3 +37,4 @@ public class Test {
         }
     }
 }
+private class gem
