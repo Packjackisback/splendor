f24d6aa275ffc826f94b736ef387377754b39abf
packjack <83289186+Packjackisback@users.noreply.github.com>
2024-11-05 07:52:25 -0600

    Implemented CSV loading
    Tested

--- Splendor/SplendorRunner.java
+++ Splendor/SplendorRunner.java
@@ -1,14 +1,16 @@
 package Splendor;
 
+import java.io.IOException;
+
 public class SplendorRunner {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException {
 
         GameFrame gameFrame = new GameFrame();
         //The following code is for testing/demonstrative purposes only and should be removed
         Game game = new Game();
         GamePanel testing = new GamePanel(game);
         gameFrame.setPanel(testing);
-
+       
     }
     public static void setUpGame() {
 
