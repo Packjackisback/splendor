ac292742461353ea767df4f0c3b2ffb05bcd2ef2
k1402233 <90855747+k1402233@users.noreply.github.com>
2024-11-08 08:02:44 -0600

    Adding methods

--- Splendor/Token.java
+++ Splendor/Token.java
@@ -1,16 +1,24 @@
-package Splendor;
-
 import java.awt.*;
 import java.awt.image.BufferedImage;
 
 public class Token extends Piece implements Drawable{
 	private Gem gem;
+	private boolean isWild;
 	
-	public Token(BufferedImage img, Gem g) {
+	public Token(BufferedImage img, Gem g, boolean wild) {
 		super("Token", img);
 		gem = g;
+		isWild = wild;
 	}
 
+	public boolean isWild() {
+		return isWild;
+	}
+	
+	public Gem getGem() {
+		return gem;
+	}
+	
 	@Override
 	public void draw(Graphics g, int x, int y, int width, int height) {
 		g.drawImage(super.getImage(), x, y, width, height, null);
