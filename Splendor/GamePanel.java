78861eb919edb7ec5fc7659190f22e639743e272
packjack <83289186+Packjackisback@users.noreply.github.com>
2024-11-13 07:16:37 -0600

    Adding startpanel and gameover

--- Splendor/GamePanel.java
+++ Splendor/GamePanel.java
@@ -5,9 +5,12 @@ import java.awt.*;
 
 public class GamePanel extends JPanel {
     private final Game game;  // Reference to the Game instance
-
-    public GamePanel(Game game) {
+    private EndPanel endPanel;
+    private GameFrame frame;
+    public GamePanel(Game game, EndPanel endPanel, GameFrame gameFrame) {
         this.game = game;  // Initialize with the Game instance
+        this.endPanel = endPanel;
+        this.frame = gameFrame;
         game.setPanel(this);
         setPreferredSize(new Dimension(1920, 1080)); // Set a preferred size
         setVisible(true);
@@ -21,5 +24,9 @@ public class GamePanel extends JPanel {
         game.drawCards(g); // Starting x, y, card width, card height, padding
         game.drawNobles(g);
         game.drawTokens(g);
+    }
+
+    public void gameOver() {
+        frame.setPanel(endPanel);
     }
 }
