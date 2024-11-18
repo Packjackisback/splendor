e2056f20183e193f25b2366428d6bc1a02f19c03
ArchithM <152109269+ArchithM@users.noreply.github.com>
2024-11-18 07:40:29 -0600

    Add files via upload

--- Splendor/mouseListener.java
+++ Splendor/mouseListener.java
@@ -1,11 +1,52 @@
 package Splendor;
-import javax.imageio.ImageIO;
-import javax.swing.*;
-import java.awt.*;
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
 import java.awt.event.MouseEvent;
 import java.awt.event.MouseListener;
-import java.awt.image.BufferedImage;
-public class mouseListener {
+import java.util.*;
+public class mouseListener implements MouseListener {
+    public Game game;
+    private Set<Token> tokenKeys;
+    private ArrayList<ArrayList<Card>> cards;
+    public mouseListener(Game game) {
+        this.game = game;
+        tokenKeys = game.getTokens().keySet();
+        cards = game.getCardArray();
+    }
+    @Override
+    public void mouseClicked(MouseEvent e) {
+        int x = e.getX();
+        int y = e.getY();
+        for (Token token : tokenKeys) {
+            if (x >= token.getX() && x <= token.getX() + token.getWidth() && y >= token.getY() && y <= token.getY() + token.getHeight()) {
+                game.takeToken(token);
+            }
+        }
+
+        for (ArrayList<Card> arr: cards) {
+            for (Card card : arr) {
+                if (x >= card.getX() && x <= card.getX() + card.getWidth() && y >= card.getY() && y <= card.getY() + card.getHeight()) {
+                    game.takeCard(card);
+                }
+            }
+        }
+    }
+
+    @Override
+    public void mousePressed(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseReleased(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseEntered(MouseEvent e) {
+
+    }
+
+    @Override
+    public void mouseExited(MouseEvent e) {
+
+    }
 }
